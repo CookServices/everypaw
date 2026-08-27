8cf76fac586b2e278c0b4132c048ea6c3fa6a2e6
julienmauduit-glitch <julien.mauduit@gmail.com>
2026-08-27 13:27:45 +0200

    feat(design-system): add Input component

---
+++ packages/design-system/src/components/Input.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  label?: string;
+}
+
+export function Input({ error, label, id, ...rest }: InputProps) {
+  return (
+    <div className="ep-input-wrapper">
+      {label && (
+        <label className="ep-input-label" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        className={`ep-input${error ? ' ep-input--error' : ''}`}
+        {...rest}
+      />
+    </div>
+  );
+}
